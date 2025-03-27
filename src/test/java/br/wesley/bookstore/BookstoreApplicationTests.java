474708ff2c4724a6f3ec0beaa986b74bf97de32d
Wesley Rosa <wesleymrosa@gmail.com>
2025-03-27 16:31:27 -0300

    Foram criados os pacotes base do projeto

---
+++ src/test/java/br/wesley/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wesley.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
